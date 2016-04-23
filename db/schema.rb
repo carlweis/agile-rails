c0ab55ea5726d6094d59ec207c1a0cd2dd7f9ed1
Carl Weis <carl@carlweis.com>
2016-04-23 07:13:54 -0500

    Add admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423113104) do
+ActiveRecord::Schema.define(version: 20160423120217) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 20160423113104) do
     t.decimal  "price",                     precision: 10
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            limit: 255
+    t.string   "password_digest", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   add_foreign_key "line_items", "carts"
